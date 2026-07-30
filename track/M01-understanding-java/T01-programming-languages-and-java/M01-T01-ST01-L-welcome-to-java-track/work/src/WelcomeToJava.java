4dcd1a9e43c49ae72db2865ed38d20f805b804fc
kavyasreechedipothula-ctrl <kavyasreechedipothula@gmail.com>
2026-07-30 13:06:54 +0530

    added one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,5 +3,7 @@ public class WelcomeToJava {
         System.out.println("Welcome to java");
         System.out.println("I already know how to build programming logic");
         System.out.println("now i am learning how to express that logic using java");
+        System.out.println("I am excited to see what i can create with java");
+        System.out.println("Let's start");
     }
 }
