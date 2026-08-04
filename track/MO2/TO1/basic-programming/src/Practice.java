e55a8c17f96f9983fe21772dc0e203870a8380c3
kavyasreechedipothula-ctrl <kavyasreechedipothula@gmail.com>
2026-08-04 11:40:40 +0530

    Added Simple Program

---
+++ track/MO2/TO1/basic-programming/src/Practice.java
@@ -0,0 +1,11 @@
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.println("Tuesday");
+        System.out.print("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+    }
+}
